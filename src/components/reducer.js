85d1a41a214b7466f4d6c735a09560a71ecb74db
omotoso eniola <omotosoeniola2@gmail.com>
2024-07-05 14:26:56 +0100

    first commit

---
+++ src/components/reducer.js
@@ -0,0 +1,54 @@
+export default function reducer(state, action, initalState) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFaild":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return { ...state, status: "active" };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highScore:
+          state.points > state.highScore ? state.points : state.highScore,
+      };
+
+    case "restart":
+      return {
+        questions: state.questions,
+        status: "ready",
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
